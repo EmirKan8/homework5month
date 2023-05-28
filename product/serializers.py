c97de3acfd3c7e7d751038e192e66a45182469ad
beishebaev01 <bshv228@icloud.com>
2023-05-28 21:43:24 +0600

    ДЗ2

--- product/serializers.py
+++ product/serializers.py
@@ -1,11 +1,5 @@
 from rest_framework import serializers
-from .models import Category, Product, Review
-
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = '__all__'
+from product.models import Category, Product, Review
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -14,7 +8,25 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class CategorySerializer(serializers.ModelSerializer):
+    product_count = ProductSerializer
+
+    class Meta:
+        model = Category
+        fields = 'name product_count'.split()
+        # fields = '__all__'
+
+
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = '__all__'
+        fields = 'text stars'.split()
+        # fields = '__all__'
+
+
+class ProductsReviewsSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True)
+
+    class Meta:
+        model = Product
+        fields = 'title reviews rating'.split()
